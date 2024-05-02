2504f861a4995fc01881a4c05423c9d1f68e230a
priyanksukhadiya <priyanksukhadiya2001@gmail.com>
2024-05-02 22:50:21 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/common/header";
+import Footer from "./components/common/footer";
+import Home from "./components/home";
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
